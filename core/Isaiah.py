62187aec6716435bf5560466431b7a6d856c19e2
Akins <82357502+Akins2229@users.noreply.github.com>
2021-11-16 16:17:29 -0600

    Update Isaiah.py

--- core/Isaiah.py
+++ core/Isaiah.py
@@ -28,6 +28,8 @@ import json
 import os
 
 from utils import __recursive_object_builder
+
+from .context import Context
 # from utils import __build_database
 
 class Isaiah(commands.AutoShardedBot):
@@ -91,4 +93,31 @@ class Isaiah(commands.AutoShardedBot):
       return self.__default_prefix
     
     else:
-      return self.db['prefixes'][str(message.guild.id)]
+      return self.db['prefixes'][str(message.guild.id)] 
+      
+  async def process_commands(self, message: Message) -> None:
+      """|coro|
+      
+      This function processes the commands that have been registered
+      to the bot and other groups. Without this coroutine, none of the
+      commands will be triggered.
+      By default, this coroutine is called inside the :func:`.on_message`
+      event. If you choose to override the :func:`.on_message` event, then
+      you should invoke this coroutine as well.
+      This is built using other low level tools, and is equivalent to a
+      call to :meth:`~.Bot.get_context` followed by a call to :meth:`~.Bot.invoke`.
+      This also checks if the message's author is a bot and doesn't
+      call :meth:`~.Bot.get_context` or :meth:`~.Bot.invoke` if so.
+      Parameters
+      -----------
+      message: :class:`discord.Message`
+          The message to process commands for.
+      """
+      if message.author.bot:
+          return
+
+      ctx = await self.get_context(message, Context)
+      await self.invoke(ctx)
+
+  async def on_message(self, message):
+      await self.process_commands(message)
