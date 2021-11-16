9d09ca8e572617692561b53ebf9b54fae1bb1da9
Akins <82357502+Akins2229@users.noreply.github.com>
2021-11-16 17:03:38 -0600

    Update __init__.py

--- plugins/core/__init__.py
+++ plugins/core/__init__.py
@@ -1,13 +1,13 @@
-from discord.ext import commands
+from core import Isaiah
 
 from plugins.core.error import ErrorHandler
 from plugins.core.startup import Login
 
-def setup(bot: commands.Bot):
+def setup(bot: Isaiah):
   """Sets up the cogs from the core module.
     
      Parameters:
-        bot: discord.ext.commands.Bot - The bot the cog is loaded onto. Passed by discord.py
+        bot: core.Isaiah - The bot the cog is loaded onto. Passed by discord.py
   """
   bot.add_cog(ErrorHandler(bot))
   bot.add_cog(Login(bot))
